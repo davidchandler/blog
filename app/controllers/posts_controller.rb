b03c742eda7cb15fa214219ccd7bb15fc9df3346
David Chandler <dchandler43@hotmail.com>
2013-08-06 09:52:27 -0700

    first commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,7 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
@@ -18,6 +19,20 @@ class PostsController < ApplicationController
       redirect_to @post
     else
       render 'new'
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+ 
+    if @post.update(params[:post].permit(:title, :text))
+      redirect_to @post
+    else
+      render 'edit'
     end
   end
 
